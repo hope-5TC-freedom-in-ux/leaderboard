91d4d7f1084898cf326a0aea732052acecc2af9f
Tas-Hur <arthur.navarro@insa-lyon.fr>
2019-09-23 19:24:11 +0100

    first commit, adding files

---
+++ vueScore.js
@@ -0,0 +1,34 @@
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    value:34,
+    username: 'Hello Vue!',
+    scores:[],
+    global_score:{}
+  },
+  methods:{
+    getUsername(){
+      $.get("/api/VO.1/username", res =>{
+        console.log(res)
+      })
+    },
+    hover(index){
+      Vue.set(this.scores[index],'hovered',true);
+    },
+    leave(index){
+      Vue.set(this.scores[index],'hovered',false);
+    }
+  },
+  mounted(){
+    console.log("MOUNTED")
+    this.username="John"
+    this.global_score={name:'Challenge 0', privacy:78,time:17}
+    this.scores=[
+      {name:'Challenge 0', privacy:78,time:17},
+      {name:'Challenge 0', privacy:78,time:17},
+      {name:'Challenge 0', privacy:78,time:17},
+      {name:'Challenge 0', privacy:78,time:17}]
+    }
+  })
